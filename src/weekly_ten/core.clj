9e796d1518833dddd90a867dd5ac4be29febc1f1
Anders Engström <epirealer@gmail.com>
2016-10-03 21:31:19 +0200

    work work

--- src/weekly_ten/core.clj
+++ src/weekly_ten/core.clj
@@ -1,7 +1,24 @@
 (ns weekly-ten.core
-  (:gen-class))
+  (:gen-class)
+  (:require [weekly-ten.server :as server]))
+
+(defn simple-query-handler
+  "Answer a query"
+  [query]
+  (apply str (reverse query)))
+
+(defn simple-protocol-handler
+  "Implement the simple protocol:
+  - When a connection is received, it must send six (6) bytes to the client: “ready\n”.
+  - It must read a 4-byte word from the client, indicating the length of the query to read.
+  - It must read that many bytes as the query, and then perform the requested query (see above).
+  - It must send a 4-byte word to the client, indicating the length of the response.
+  - It must send the response."
+  [in out]
+  (.write out (into-array Byte/TYPE (seq "ready\n"))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (if (= (first args) "server")
+    (weekly-ten.server/start simple-protocol-handler 9999)
+    (throw (Exception. "Noop"))))
--- src/weekly_ten/server.clj
+++ src/weekly_ten/server.clj
@@ -1,35 +1,34 @@
 (ns weekly-ten.server
   "The server part. Implemented without any third-party libs. All std java and Clojure."
-  (:import (java.net ServerSocket)))
-
-(defn array->string "Takes a char-array and returns it as a string"
-  ([arr] (array->string arr (count arr)))
-  ([arr n] (apply str (take n (seq arr)))))
+  (:require [clojure.tools.logging :as log])
+  (:import java.net.ServerSocket))
 
 (defn thread!
   "Run something in a thread"
   [fun & args]
   (.start (Thread. #(apply fun args))))
 
-(defn dummy [in out]
-  (let [buf (make-array Character/TYPE 4)
-        n-read (.read in buf)
-        data (array->string buf n-read)]
-    (.write out (str "Hello " data))
-    (.flush out)))
-
 (defn handle-request [client-socket handler-fn]
+  (log/infof "Processing request from %s with %s" client-socket handler-fn)
   (with-open [client client-socket
-              in (clojure.java.io/reader (.getInputStream client))
-              out (clojure.java.io/writer (.getOutputStream client))]
+              in (clojure.java.io/input-stream (.getInputStream client))
+              out (clojure.java.io/output-stream (.getOutputStream client))]
     (handler-fn in out)))
 
-(defn run [port]
-  (with-open [server (ServerSocket. (int port))]
-    (loop [server server]
-      (let [client (.accept server)]
-        (thread! handle-request client dummy)
-        (recur server)))))
+(defn start [handler-fn port]
+  (log/infof "Starting server on port %d" port)
+  (let [server (ServerSocket. (int port))
+        poll-fn (fn [s]
+                  (log/info "Starting client handler")
+                  (let [client (.accept s)]
+                    (log/infof "Client connected as %s" client)
+                    (thread! handle-request client handler-fn)
+                    (recur s)))]
+    (thread! poll-fn server)
+    server))
 
-
-
+(defn stop [server]
+  (when server
+    (do
+      (log/infof "Closing server %s" server)
+      (.close server))))
