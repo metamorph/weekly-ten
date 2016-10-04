e64882e42736137a27d4a0942aa728dd239dd1ca
Anders Engström <epirealer@gmail.com>
2016-10-04 23:54:44 +0200

    Ohoj - the protocol seems to work
    Still need to fix the server. After that, part one is done.

--- test/weekly_ten/core_test.clj
+++ test/weekly_ten/core_test.clj
@@ -2,17 +2,21 @@
   (:require [clojure.test :refer :all]
             [weekly-ten.core :refer :all]))
 
-
 (defn query-client [query]
   (let [out  (java.io.PipedOutputStream.)
-        in   (java.io.ByteArrayOutputStream.)
-        sout (java.io.PipedInputStream. out)]
+        in   (java.io.PipedInputStream.)
+        sin  (java.io.PipedInputStream. out)
+        sout (java.io.PipedOutputStream. in)]
 
-    (.start (Thread. (fn [] (simple-protocol-handler sout in))))
+    (.start (Thread. (fn [] (simple-protocol-handler sin sout))))
 
     ;; Implement the client protocol
-    ;; Read the ready string.
-    (let [ready (bytes->string (read-bytes in 6))]
-      (prn ready))))
+    (let [ready       (bytes->string (read-bytes in 6))
+          bytes-query (string->bytes query)]
+      (.write out (int->word4 (count bytes-query)))
+      (.write out bytes-query)
+      (let [answer-len   (word4->int (read-bytes in 4))
+            answer-bytes (read-bytes in answer-len)]
+        (bytes->string answer-bytes)))))
 
 
